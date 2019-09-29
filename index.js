6e7e396bc5344d104f3c9d53632cf96fe62a75ef
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-28 23:25:57 -0500

    added correct middleware function and server port

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code away!
+
+const server = require('./server');
+
+server.listen(6000, () => console.log('Server listening on port 6000'));
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +13,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+console.log(req.method, req.url, Date.now());
+next();
 };
 
 module.exports = server;
