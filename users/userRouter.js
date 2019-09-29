8e22da4e485ccbe9b41a21a7da43eeb661adfe14
Patrick Gordon <patrickgordon42@yahoo.com>
2019-09-29 00:31:33 -0600

    did put and getById for users

--- users/userRouter.js
+++ users/userRouter.js
@@ -23,11 +23,22 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    User.getById(id)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(400).json({error: 'The user with this id could not be found'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'User info could not be found'});
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
-
 });
 
 router.delete('/:id', (req, res) => {
@@ -35,7 +46,23 @@ router.delete('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-
+    const { id } = req.params;
+    const { name } = req.body;
+    User.getById(id)
+    .then(user => {
+        if (user) {
+            User.update(id, {name})
+            .then(updated => {
+                res.status(200).json(updated)
+            })
+        } else {
+            res.status(400).json({error: 'user with this id could not be found'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'error with the server getting id'})
+    })
 });
 
 //custom middleware
